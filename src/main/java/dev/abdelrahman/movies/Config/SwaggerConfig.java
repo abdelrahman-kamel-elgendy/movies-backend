2bfa46c2c4a348c7bd8ef3eb8dc3217dbda46391
abdelrahman-kamell <abdelrahman.kamel@si-ware.com>
2025-08-06 15:48:06 +0300

    Integrated with Swagger

---
+++ src/main/java/dev/abdelrahman/movies/Config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package dev.abdelrahman.movies.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI moviesOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Movies API")
+                        .description("A RESTful API for managing movies and reviews")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Abdelrahman")
+                                .email("abdelrahman@example.com")));
+    }
+} 
