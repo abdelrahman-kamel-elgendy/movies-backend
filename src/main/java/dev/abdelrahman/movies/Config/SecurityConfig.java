9f42af48ae8c3e88926927ef928bf0b6732f1ece
abdelrahman-kamell <abdelrahman.kamel@si-ware.com>
2025-08-06 20:07:24 +0300

    Refactor SecurityConfig and authController: remove unused JwtAuthenticationFilter and related imports, streamline authController by removing unnecessary authentication logic, and return a bad request response for signin endpoint.

--- src/main/java/dev/abdelrahman/movies/Config/SecurityConfig.java
+++ src/main/java/dev/abdelrahman/movies/Config/SecurityConfig.java
@@ -12,14 +12,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     
-    @Autowired
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
     
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,7 +29,6 @@ public class SecurityConfig {
             })
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
--- src/main/java/dev/abdelrahman/movies/Controllers/authController.java
+++ src/main/java/dev/abdelrahman/movies/Controllers/authController.java
@@ -13,11 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.abdelrahman.movies.Models.User.User;
 import dev.abdelrahman.movies.Models.User.DTOs.SigninDTO;
 import dev.abdelrahman.movies.Models.User.DTOs.SignupDTO;
-import dev.abdelrahman.movies.Models.User.DTOs.JwtResponse;
 import dev.abdelrahman.movies.Services.AuthService;
 import dev.abdelrahman.movies.Utils.MessageResponse;
-import dev.abdelrahman.movies.Utils.JwtUtils;
 import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,24 +25,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class authController {
 
     @Autowired
-    private AuthService authService;    
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private JwtUtils jwtUtils;
-
-
+    private AuthService authService;
 
     @PostMapping("/signin")
     public ResponseEntity<?> signin(@Valid @RequestBody SigninDTO signinDTO) {
-        Authentication authentication = authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(signinDTO.getUsername(), signinDTO.getPassword())
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
         
-        String jwt = jwtUtils.generateToken(authentication);
-        
-        return ResponseEntity.ok(new JwtResponse(jwt, signinDTO.getUsername()));
+        return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
     }
     
     @PostMapping("/signup")
