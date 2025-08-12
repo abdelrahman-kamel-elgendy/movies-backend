14e0c43b6e9a6cbfed0d6855f1a735c84989318c
abdelrahman-kamell <abdelrahman.kamel@si-ware.com>
2025-08-12 14:46:53 +0300

    Refactor ProfileController: remove unused imports to enhance code clarity and maintainability.

--- src/main/java/dev/abdelrahman/movies/Controllers/ProfileController.java
+++ src/main/java/dev/abdelrahman/movies/Controllers/ProfileController.java
@@ -1,10 +1,7 @@
 package dev.abdelrahman.movies.Controllers;
 
-import java.net.http.HttpResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
