dd643fb3693d9fc91687ee80b7d8486ab8affbc1
abdelrahman-kamell <abdelrahman.kamel@si-ware.com>
2025-08-16 22:01:03 +0300

    add swagger Security Item

--- src/main/java/dev/abdelrahman/movies/Config/SwaggerConfig.java
+++ src/main/java/dev/abdelrahman/movies/Config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package dev.abdelrahman.movies.Config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +20,13 @@ public class SwaggerConfig {
                         .version("1.0.0")
                         .contact(new Contact()
                                 .name("Abdelrahman")
-                                .email("abdelrahman.kamel.elgendy@gmail.com")));
+                                .email("abdelrahman.kamel.elgendy@gmail.com")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 } 
